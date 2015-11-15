8c7c28df9044c62b6262bfb494e5e9d6cf2490a4
MarBle <markusblechschmidt@gmail.com>
2015-11-15 16:14:25 +0100

    change folder name

---
+++ A4x0moeglicheLoesung/I.java
@@ -0,0 +1,12 @@
+
+/**
+ * Sinnbefreite Test-Klasse um Vererbung zu ueben
+ * 
+ * @author   Michael Schaefers 
+ * @version  2015/11/10
+ */
+interface I {                                               // Interface ist automatisch public abstract
+    
+    void om();                                              // Methode ist automatisch public abstract
+    
+}// interface
